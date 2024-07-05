4efdd9a528b26d00bcb1e0e64d6526b6830ed5e8
tianzeteam <kedao2000@gmail.com>
2024-07-05 12:56:06 +0800

    Delete src/initializeGlobals.js

--- src/initializeGlobals.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// initializeGlobals.ts
-const https_proxy_agent_1 = require("https-proxy-agent");
-// 初始化全局变量
-global.proxyAgent = new https_proxy_agent_1.HttpsProxyAgent('http://127.0.0.1:7890');
